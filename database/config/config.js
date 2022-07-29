69645a74d3be2a11e5f1348e3ffe2a67702a5333
Felipe <felipe.creis@icloud.com>
2022-07-28 21:26:40 -0300

    Alterações
    Co-authored-by: Milléo <Milleo@users.noreply.github.com>

--- database/config/config.js
+++ database/config/config.js
@@ -4,7 +4,8 @@ module.exports= {
     "password": "",
     "database": "fonesempre_db",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": require("mysql2")
   },
   "test": {
     "username": "root",
