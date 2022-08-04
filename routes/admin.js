649bc0884a2fa5419479c61bfe3beac51c28e504
Igor Muller <muller.igor@icloud.com>
2022-08-04 16:12:12 -0300

    Exclusão controllers em desuso

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const AdministracaoController = require('../controllers/AdministracaoController'
 
 router.get("/admin/dashboard", AdministracaoController.dashboard);
 
+router.post("/admin/produtos/excluir", AdministracaoController.excluir);
+
 
 
 module.exports = router;    
